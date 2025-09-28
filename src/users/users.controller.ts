e8b79e0bcd1e11c0cd31e343f6ffe01448487030
aya hussein <aya.hussein3107@gmail.com>
2025-09-28 22:32:10 +0300

    add findUserByEmail endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,32 @@
-import { Get, Controller, UseGuards, Req } from '@nestjs/common';
+import { Get, Controller, UseGuards, Req, Query, NotFoundException } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
+import {UsersService} from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(
+        private readonly usersService: UsersService,
+
+    ) {}
 
     @UseGuards(AuthGuard)
     @Get('profile')
     async profile(@Req() req: Request) {
         return req['user'];        
+    }
+
+    @Get()
+    async findOneByEmail(@Query('email') email?: string) {
+      if (!email) {
+        throw new NotFoundException('Email query parameter is required');
+      }
+  
+      const user = await this.usersService.findUserByEmail(email);
+      if (!user) {
+        throw new NotFoundException(`User with email ${email} not found`);
+      }
+  
+      return user;
     }
 
 
